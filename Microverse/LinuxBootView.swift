8f6a6d31d9f3b5862c241a752c4ad1dc6bd40fc9
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-08-02 20:28:36 +0100

    Add view for configuring Linux boot

---
+++ Microverse/LinuxBootView.swift
@@ -0,0 +1,39 @@
+//
+//  LinuxBootView.swift
+//  LinuxBootView
+//
+//  Created by Justin Spahr-Summers on 02/08/2021.
+//
+
+import SwiftUI
+
+struct LinuxBootViewModel {
+    var kernelPath = ""
+    var commandLine = ""
+    var initialRamdiskPath = ""
+}
+
+struct LinuxBootView: View {
+    @Binding var viewModel: LinuxBootViewModel
+    
+    var body: some View {
+        Form {
+            TextField("Path to Kernel", text: $viewModel.kernelPath)
+            TextField("Path to initial RAM disk", text: $viewModel.initialRamdiskPath)
+            TextField("Command line arguments", text: $viewModel.commandLine)
+        }
+    }
+}
+
+struct LinuxBootView_Previews: PreviewProvider {
+    struct Holder: View {
+        @State var viewModel = LinuxBootViewModel()
+        var body: some View {
+            return LinuxBootView(viewModel: $viewModel)
+        }
+    }
+    
+    static var previews: some View {
+        Holder()
+    }
+}
