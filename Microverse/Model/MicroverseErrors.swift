75f9fbb9c9611a041e268f04fa7dbb761d7b621e
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-08-08 12:14:34 +0100

    VirtualMachineController class

---
+++ Microverse/Model/MicroverseErrors.swift
@@ -0,0 +1,16 @@
+//
+//  MicroverseErrors.swift
+//  MicroverseErrors
+//
+//  Created by Justin Spahr-Summers on 08/08/2021.
+//
+
+import Foundation
+
+enum MicroverseError: Error {
+    
+    var localizedDescription: String {
+        switch self {
+        }
+    }
+}
---
+++ Microverse/Model/VirtualMachineController.swift
@@ -0,0 +1,27 @@
+//
+//  VirtualMachineController.swift
+//  VirtualMachineController
+//
+//  Created by Justin Spahr-Summers on 08/08/2021.
+//
+
+import Foundation
+import Virtualization
+
+final class VirtualMachineController: NSObject, VZVirtualMachineDelegate {
+    let dispatchQueue = DispatchQueue(label: "com.metacognitive.Microverse.VirtualMachineController", qos: .userInitiated, attributes: .init(), autoreleaseFrequency: .workItem)
+    let virtualMachine: VZVirtualMachine
+    
+    init(configuration: VZVirtualMachineConfiguration) throws {
+        try configuration.validate()
+        virtualMachine = VZVirtualMachine(configuration: configuration, queue: dispatchQueue)
+    }
+    
+    func guestDidStop(_ virtualMachine: VZVirtualMachine) {
+        
+    }
+    
+    func virtualMachine(_ virtualMachine: VZVirtualMachine, didStopWithError error: Error) {
+        
+    }
+}
