880fbf925b443decfb15717d4a61e9922af02495
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-08-09 19:19:42 +0100

    Manually synchronise installation progress updates
    It seems like ProgressView isn't thread-safe with respect to its provided Progress object (and, AFAICT, there's no way to specify the thread that a Progress delivers updates upon).
    
    Resolves #23.

--- Microverse/View/MacOSInstallView.swift
+++ Microverse/View/MacOSInstallView.swift
@@ -5,6 +5,7 @@
 //  Created by Justin Spahr-Summers on 07/08/2021.
 //
 
+import Combine
 import SwiftUI
 import Virtualization
 
@@ -14,14 +15,19 @@ struct MacOSInstallView: View {
     var virtualMachineController: VirtualMachineController
     var restoreImageURL: URL
     @State var installer: VZMacOSInstaller? = nil
+    @State var progress = 0.0
     var action: () -> Void
     
     var body: some View {
         GroupBox("macOS Installation") {
             HStack {
                 Form {
-                    if let installer = installer {
-                        ProgressView(installer.progress)
+                    if installer != nil {
+                        ProgressView(value: progress) {
+                            Text("Installing…")
+                        } currentValueLabel: {
+                            Text("\(Int(progress * 100))% completed")
+                        }
                     } else {
                         Button("Start") {
                             virtualMachineController.dispatchQueue.async {
@@ -29,10 +35,20 @@ struct MacOSInstallView: View {
 
                                 DispatchQueue.main.async {
                                     self.installer = installer
+                                    self.progress = 0.0
                                 }
+                                
+                                let cancellable = installer.progress.publisher(for: \.completedUnitCount)
+                                    .map { completed in Double(completed) / Double(installer.progress.totalUnitCount) }
+                                    .receive(on: DispatchQueue.main)
+                                    .sink { progress in
+                                        self.progress = progress
+                                    }
 
                                 NSLog("Starting installation into \(virtualMachineController) from \(restoreImageURL)")
-                                installer.install { result in
+                                installer.install { [cancellable] result in
+                                    cancellable.cancel()
+                                    
                                     DispatchQueue.main.async {
                                         switch result {
                                         case .success:
