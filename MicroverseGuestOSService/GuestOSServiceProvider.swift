6f6522ec7dab54881fcee338d902e0d2d8207d82
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-08-10 11:57:07 -0700

    Implement guest OS service stub

--- MicroverseGuestOSService/GuestOSServiceProvider.swift
+++ MicroverseGuestOSService/GuestOSServiceProvider.swift
@@ -5,8 +5,18 @@
 //  Created by test on 10/08/2021.
 //
 
+import AppKit
 import Foundation
+import GRPC
+import NIO
 
 final class GuestOSServiceProvider: Microverse_GuestOSServiceProvider {
+    var interceptors: Microverse_GuestOSServiceServerInterceptorFactoryProtocol?
     
+    func paste(request: Microverse_PasteRequest, context: StatusOnlyCallContext) -> EventLoopFuture<Microverse_PasteResponse> {
+        let pboard = NSPasteboard.general
+        pboard.clearContents()
+        pboard.setData(request.content.data(using: .utf8), forType: .string)
+        return context.eventLoop.makeSucceededFuture(Microverse_PasteResponse.with { $0.success = true })
+    }
 }
--- MicroverseGuestOSService/main.swift
+++ MicroverseGuestOSService/main.swift
@@ -6,6 +6,25 @@
 //
 
 import Foundation
+import GRPC
+import NIO
 
-print("Hello, World!")
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+defer {
+    try! group.syncShutdownGracefully()
+}
 
+let provider = GuestOSServiceProvider()
+let server = Server.insecure(group: group)
+    .withServiceProviders([provider])
+    .bind(host: "localhost", port: 6622)
+
+server.map {
+    $0.channel.localAddress
+}.whenSuccess { address in
+    print("Server started: \(address!)")
+}
+
+_ = try server.flatMap {
+    $0.onClose
+}.wait()
