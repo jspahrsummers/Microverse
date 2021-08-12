9e1138c49b31a3a9f2be9d9cf8b7f40a6491ab72
Justin Spahr-Summers <justin@jspahrsummers.com>
2021-08-12 09:11:29 +0100

    Copy launchd plist to disk image too

--- MicroverseGuestOSService/package_installer.sh
+++ MicroverseGuestOSService/package_installer.sh
@@ -5,6 +5,8 @@ set -o errexit
 set -x
 
 binary="$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME"
+launchd_plist="$SRCROOT/MicroverseGuestOSService/com.metacognitive.Microverse.GuestOSService.plist"
+
 image_folder="$TEMP_FILES_DIR/MicroverseGuestOSServices"
 intermediate_image="$TEMP_FILES_DIR/MicroverseGuestOSServices-intermediate.dmg"
 final_image="$DERIVED_FILE_DIR/MicroverseGuestOSServices.dmg"
@@ -12,6 +14,8 @@ volume_name="Microverse Guest OS Services"
 
 rm -rf "$image_folder" "$intermediate_image" "$final_image"
 mkdir -p "$image_folder"
-cp "$binary" "$image_folder/"
+
+cp -R "$binary" "$launchd_plist" "$image_folder/"
+
 hdiutil create -srcfolder "$image_folder" -volname "$volume_name" "$intermediate_image"
 hdiutil convert "$intermediate_image" -format UDRW -o "$final_image"
